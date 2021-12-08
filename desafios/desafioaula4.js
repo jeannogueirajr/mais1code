60e4c9546da07bc3ed456ebf430e24d0cfb7f72f
Jean Nogueira <jeannogueirajr@gmail.com>
2021-12-08 00:30:28 -0300

    atualizando os desafios

---
+++ desafios/desafioaula4.js
@@ -0,0 +1,27 @@
+// função de 2 grau
+// Ax² + Bx + c = 0
+// vcs vão pegar os valores A, B, C e resolver a equação do segundo grau
+// exemplo https://matematicabasica.net/equacao-do-2-grau-segundo-grau/
+// usar essa equação para testar: x²-5x+6=0
+// dica: a=1 b=-5 c=6
+// resultado 
+// x1 = 3
+// x2 = 2
+
+// dar um console.log no x1 e no x2 que for cvalculado pela função de vcs
+
+
+// 
+
+
+function eqsegundoGrau(a,b,c) {
+    const delta = Math.pow (b,2) - 4 * a * c;
+    const x1 = (-b + Math.sqrt(delta))/(2*a);
+    const x2 = (-b - Math.sqrt(delta))/(2*a);
+    
+console.log(x1);
+console.log(x2);
+}
+
+eqsegundoGrau(1,-5,6)
+
